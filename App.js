6c73abdd08865f07b069703c180b1e904bbccf8e
TRAN_QUANG_DO <do.tranquang@neo-career.co.jp>
2021-07-26 03:07:25 +0900

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,112 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import { Body, Button, Container, Footer, FooterTab, Form, Header, Input, Item, Label, Left, Right, Title } from 'native-base';
+// import React, { Component } from 'react';
+// import { Text } from 'react-native';
+// export default class App extends Component {
+//   render() {
+//     return (
+//       <Container>
+//         <Header>
+//           <Left>
+//             <Button transparent>
+//               <Text>abcd</Text>
+//             </Button>
+//           </Left>
+//           <Body>
+//             <Title>Header</Title>
+//           </Body>
+//           <Right />
+//         </Header>
+//         <Container>
+//           <Form>
+//             <Item>
+//               <Label>Username</Label>
+//               <Input />
+//             </Item>
+//           </Form>
+//         </Container>
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+//         <Footer>
+//           <FooterTab>
+//             <Button full>
+//               <Text style={{ color: 'black' }}>Footer</Text>
+//             </Button>
+//           </FooterTab>
+//         </Footer>
+//       </Container>
+//     );
+//   }
+// }
+
+import {
+  Body, Button, Container, Form, Header, Icon, Left,
+  Picker, Right, Title
+} from 'native-base';
+import React, { Component } from 'react';
+
+export default class NativeBase extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      staBranches: [
+        { province: 'province 1', id: 1 },
+        { province: 'province 2', id: 2 },
+        { province: 'province 3', id: 3 },
+        { province: 'province 4', id: 4 },
+        { province: 'province 5', id: 5 },
+      ],
+      staSelectedBranch: 0,
+    };
+  }
+
+  _branchSelected(arg_value) {
+    this.setState({
+      staSelectedBranch: arg_value,
+    });
+  }
+
+  render() {
+    const { staBranches, staSelectedBranch } = this.state
+    return (
+      <Container>
+        <Header>
+          <Left>
+            <Button
+              transparent
+              onPress={() => { alert('BACK!') }}
+            >
+              <Icon name="arrow-back" />
+            </Button>
+          </Left>
+          <Body>
+            <Title>Picker</Title>
+          </Body>
+          <Right />
+        </Header>
+        <Container>
+          <Form>
+            <Picker
+              style={{ width: 300, height: 40 }}
+              iosHeader="Branch"
+              Header="Branch"
+              mode="dropdown"
+              textStyle={{ color: 'grey' }}
+              placeholder="Click here to select branch"
+              headerBackButtonText="Back"
+              selectedValue={staSelectedBranch}
+              onValueChange={value => this._branchSelected(value)}>
+              {staBranches.map((item_branches, i) => {
+                return (
+                  <Picker.Item
+                    label={`You chose: ${item_branches.province}`}
+                    value={item_branches.id}
+                    key={i}
+                  />
+                );
+              })}
+            </Picker>
+          </Form>
+        </Container>
+      </Container>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ README.md
@@ -0,0 +1,25 @@
+# rn-native-base-demo 🐳
+https://docs.nativebase.io/
+
+![Stars](https://img.shields.io/github/stars/tquangdo/rn-native-base-demo?color=f05340)
+![Issues](https://img.shields.io/github/issues/tquangdo/rn-native-base-demo?color=f05340)
+![Forks](https://img.shields.io/github/forks/tquangdo/rn-native-base-demo?color=f05340)
+[![Report an issue](https://img.shields.io/badge/Support-Issues-green)](https://github.com/tquangdo/rn-native-base-demo/issues/new)
+
+## usage
+scan bar code on `expo go` appstore
+
+## version
+- `package.json`
+```json
+"expo": "~42.0.1",
+"native-base": "2.13.8",
+```
+>`npm i --save-exact native-base@2.13.8`
+
+## demos app
+![1](screenshots/1.jpeg)
+************************
+![2](screenshots/2.jpeg)
+************************
+![3](screenshots/3.jpeg)
