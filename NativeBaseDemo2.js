d84b900a6b23705b8944cd955ffaa3e276a3ca2a
TRAN_QUANG_DO <do.tranquang@neo-career.co.jp>
2021-08-23 00:53:48 +0900

    edit screenshot

--- NativeBaseDemo2.js
+++ NativeBaseDemo2.js
@@ -1,19 +1,22 @@
 import { Body, Button, Card, CardItem, Icon, Left, Text, Thumbnail } from 'native-base';
 import React, { Component } from 'react';
 import { Image, SafeAreaView, ScrollView, StatusBar } from 'react-native';
-export default class NativeBaseDemo2 extends Component {
+export default class CardComponent extends Component {
   render() {
     return <SafeAreaView>
       <StatusBar barStyle='dark-content' />
       <ScrollView>
         <Card>
+          <CardItem style={{ justifyContent: 'center', }} header bordered>
+            <Text>Profile</Text>
+          </CardItem>
           <CardItem>
             <Left>
               <Thumbnail source={{
                 uri: 'https://reactnative.dev/img/tiny_logo.png',
               }} />
               <Body>
-                <Text>This is my profile</Text>
+                <Text>DoTQ</Text>
                 <Text note>23 February, 2018</Text>
               </Body>
             </Left>
@@ -42,21 +45,21 @@ export default class NativeBaseDemo2 extends Component {
               </Button>
             </Left>
           </CardItem>
-          <CardItem style={{ height: 60 }}>
+          <CardItem style={{ height: 60 }} footer bordered>
             <Text>1000 likes</Text>
           </CardItem>
           <CardItem>
             <Body>
               <Text>
-                I am <Text style={{ fontWeight: 'bold' }}>Nguyen Duc Hoang</Text>, I live in Hanoi, Vietnam.
+                I am <Text style={{ fontWeight: 'bold' }}>DoTQ</Text>, I live in HCM, Vietnam.
                 I create this channel which contains videos in Swift programming language, python, NodeJS, Angular, Typescript,  ReactJS, React Native, ios and android programming, Kotlin programming, new technologies' overviews. These videos will help people learn latest programming language and software framework. They will be also very useful for their work or business.
                 My channel's languages are English and Vietnamese. Contents are updated continuously.
                 Thank you very much and hope to receive many advices for better next videos
-                I am <Text style={{ fontWeight: 'bold' }}>Nguyen Duc Hoang</Text>, I live in Hanoi, Vietnam.
+                I am <Text style={{ fontWeight: 'bold' }}>DoTQ</Text>, I live in HCM, Vietnam.
                 I create this channel which contains videos in Swift programming language, python, NodeJS, Angular, Typescript,  ReactJS, React Native, ios and android programming, Kotlin programming, new technologies' overviews. These videos will help people learn latest programming language and software framework. They will be also very useful for their work or business.
                 My channel's languages are English and Vietnamese. Contents are updated continuously.
                 Thank you very much and hope to receive many advices for better next videos
-                I am <Text style={{ fontWeight: 'bold' }}>Nguyen Duc Hoang</Text>, I live in Hanoi, Vietnam.
+                I am <Text style={{ fontWeight: 'bold' }}>DoTQ</Text>, I live in HCM, Vietnam.
                 I create this channel which contains videos in Swift programming language, python, NodeJS, Angular, Typescript,  ReactJS, React Native, ios and android programming, Kotlin programming, new technologies' overviews. These videos will help people learn latest programming language and software framework. They will be also very useful for their work or business.
                 My channel's languages are English and Vietnamese. Contents are updated continuously.
                 Thank you very much and hope to receive many advices for better next videos
